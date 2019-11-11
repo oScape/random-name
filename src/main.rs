c95ce1657f22f7c9e3082a223529380386bf714a
oScape <benoit.chassignol@gmail.com>
2019-11-12 00:28:43 +0100

    chat instead of ping

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,102 @@
 use futures::{future, prelude::*};
 use libp2p::{
+    floodsub::{self, Floodsub, FloodsubEvent},
     identity,
-    ping::{Ping, PingConfig},
-    PeerId, Swarm,
+    mdns::{Mdns, MdnsEvent},
+    swarm::NetworkBehaviourEventProcess,
+    tokio_codec::{FramedRead, LinesCodec},
+    tokio_io::{AsyncRead, AsyncWrite},
+    NetworkBehaviour, PeerId, Swarm,
 };
 use std::env;
 
-fn main() {
-    let id_keys = identity::Keypair::generate_ed25519();
+#[derive(NetworkBehaviour)]
+struct MyBehaviour<TSubstream: AsyncRead + AsyncWrite> {
+    floodsub: Floodsub<TSubstream>,
+    mdns: Mdns<TSubstream>,
+}
 
-    let peer_id = PeerId::from(id_keys.public());
-    println!("Local peer id: {:?}", peer_id);
-
-    let transports = libp2p::build_development_transport(id_keys);
-
-    let behaviour = Ping::new(PingConfig::new().with_keep_alive(true));
-
-    let mut swarm = Swarm::new(transports, behaviour, peer_id);
-
-    if let Some(addr) = env::args().nth(1) {
-        let remote_addr = addr.clone();
-        match addr.parse() {
-            Ok(remote) => match Swarm::dial_addr(&mut swarm, remote) {
-                Ok(()) => println!("Dialed {:?}", remote_addr),
-                Err(e) => println!("Dialing {:?}, failed with {:?}", remote_addr, e),
-            },
-            Err(err) => println!("Failed to parse address to dial {:?}", err),
+impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<MdnsEvent>
+    for MyBehaviour<TSubstream>
+{
+    fn inject_event(&mut self, event: MdnsEvent) {
+        match event {
+            MdnsEvent::Discovered(list) => {
+                for (peer, _) in list {
+                    self.floodsub.add_node_to_partial_view(peer)
+                }
+            }
+            MdnsEvent::Expired(list) => {
+                for (peer, _) in list {
+                    if !self.mdns.has_node(&peer) {
+                        self.floodsub.remove_node_from_partial_view(&peer)
+                    }
+                }
+            }
         }
     }
+}
+
+impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<FloodsubEvent>
+    for MyBehaviour<TSubstream>
+{
+    fn inject_event(&mut self, message: FloodsubEvent) {
+        if let FloodsubEvent::Message(message) = message {
+            println!(
+                "Received: '{:?}' from {:?}",
+                String::from_utf8_lossy(&message.data),
+                message.source
+            )
+        }
+    }
+}
+
+fn main() {
+    let id_keys = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(id_keys.public());
+
+    let transport = libp2p::build_development_transport(id_keys);
+
+    let floodsub_topic = floodsub::TopicBuilder::new("chat").build();
+
+    let mut behaviour = MyBehaviour {
+        floodsub: Floodsub::new(local_peer_id.clone()),
+        mdns: Mdns::new().expect("Failed to create mDNS service"),
+    };
+    behaviour.floodsub.subscribe(floodsub_topic.clone());
+
+    let mut swarm = Swarm::new(transport, behaviour, local_peer_id);
+
+    if let Some(to_dial) = env::args().nth(1) {
+        let dialing = to_dial.clone();
+        match to_dial.parse() {
+            Ok(to_dial) => match Swarm::dial_addr(&mut swarm, to_dial) {
+                Ok(()) => println!("Dialed {:?}", dialing),
+                Err(e) => println!("Dial {:?} failed: {:?}", dialing, e),
+            },
+            Err(err) => println!("Failed to parse address to dial: {:?}", err),
+        }
+    }
+
+    let stdin = tokio_stdin_stdout::stdin(0);
+    let mut framed_stdin = FramedRead::new(stdin, LinesCodec::new());
 
     Swarm::listen_on(&mut swarm, "/ip4/0.0.0.0/tcp/0".parse().unwrap()).unwrap();
 
     let mut listening = false;
     tokio::run(future::poll_fn(move || -> Result<_, ()> {
         loop {
+            match framed_stdin.poll().expect("Error while polling stdin") {
+                Async::Ready(Some(line)) => {
+                    swarm.floodsub.publish(&floodsub_topic, line.as_bytes())
+                }
+                Async::Ready(None) => panic!("Stdin closed"),
+                Async::NotReady => break,
+            };
+        }
+        loop {
             match swarm.poll().expect("Error while polling swarm") {
-                Async::Ready(Some(e)) => println!("{:?}", e),
+                Async::Ready(Some(_)) => {}
                 Async::Ready(None) | Async::NotReady => {
                     if !listening {
                         if let Some(a) = Swarm::listeners(&swarm).next() {
@@ -43,9 +104,11 @@ fn main() {
                             listening = true;
                         }
                     }
-                    return Ok(Async::NotReady);
+                    break;
                 }
             }
         }
+
+        Ok(Async::NotReady)
     }));
 }
